01df9c9fda53769847e9e1f3a5777ff150df5277
OJailson17 <JaylsonO17@gmail.com>
2021-12-09 09:46:36 -0300

    adding countries component to main section

---
+++ src/components/CountryComponent/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export function CountryComponent() {
+  return (
+    <div className={styles.container}>
+      <div className="imageContainer">
+        <Image
+          src="https://via.placeholder.com/320x200"
+          alt="Country flag"
+          layout="fill"
+          objectFit="cover"
+          priority
+        />
+      </div>
+      <div className="countryInfo">
+        <strong>Germany</strong>
+        <p>
+          Population: <span>81,770,000</span>
+        </p>
+        <p>
+          Region: <span>Europe</span>
+        </p>
+        <p>
+          Capital: <span>Berlin</span>
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,23 @@
+import { CountryComponent } from "../components/CountryComponent";
 import { FilterArea } from "../components/Filter";
 
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
-  return <FilterArea />;
+  return (
+    <>
+      <FilterArea />
+
+      <main className={styles.container}>
+        <CountryComponent />
+        <CountryComponent />
+        <CountryComponent />
+        <CountryComponent />
+        <CountryComponent />
+        <CountryComponent />
+        <CountryComponent />
+        <CountryComponent />
+      </main>
+    </>
+  );
 }
